7394dfa180d8070a44dba1b6bed743f92b5eee81
eunjae-lee <karis612@gmail.com>
2021-01-21 16:52:43 +0100

    move ais-instant-search-ssr to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,100 @@
 <template>
   <div>
-    <Nuxt />
+    <ais-instant-search-ssr>
+      <Nuxt />
+    </ais-instant-search-ssr>
   </div>
 </template>
 
+<script>
+import { AisInstantSearchSsr, createServerRootMixin } from "vue-instantsearch";
+import algoliasearch from "algoliasearch/lite";
+
+const searchClient = algoliasearch(
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
+);
+
+function nuxtRouter(vueRouter) {
+  return {
+    read() {
+      return vueRouter.currentRoute.query;
+    },
+    write(routeState) {
+      vueRouter.push({
+        query: routeState
+      });
+    },
+    createURL(routeState) {
+      return vueRouter.resolve({
+        query: routeState
+      }).href;
+    },
+    onUpdate(cb) {
+      if (typeof window === "undefined") return;
+
+      this._onPopState = event => {
+        const routeState = event.state;
+        // On initial load, the state is read from the URL without
+        // update. Therefore, the state object isn't present. In this
+        // case, we fallback and read the URL.
+        if (!routeState) {
+          cb(this.read());
+        } else {
+          cb(routeState);
+        }
+      };
+      window.addEventListener("popstate", this._onPopState);
+    },
+    dispose() {
+      if (typeof window === "undefined") return;
+
+      window.removeEventListener("popstate", this._onPopState);
+    }
+  };
+}
+
+export default {
+  components: {
+    AisInstantSearchSsr,
+    createServerRootMixin
+  },
+  data() {
+    const mixin = createServerRootMixin({
+      searchClient,
+      indexName: "instant_search",
+      routing: {
+        router: nuxtRouter(this.$router)
+      }
+    });
+    return {
+      ...mixin.data()
+    };
+  },
+  provide() {
+    return {
+      // Provide the InstantSearch instance for SSR
+      $_ais_ssrInstantSearchInstance: this.instantsearch
+    };
+  },
+  serverPrefetch() {
+    return this.instantsearch.findResultsState(this).then(algoliaState => {
+      this.$ssrContext.nuxt.algoliaState = algoliaState;
+    });
+  },
+  beforeMount() {
+    const results =
+      this.$nuxt.context.nuxtState.algoliaState || window.__NUXT__.algoliaState;
+
+    this.instantsearch.hydrate(results);
+  }
+};
+</script>
+
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
