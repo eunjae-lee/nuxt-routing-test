2f26b50910e19622e23cd1047c42e5fa4c0919d4
eunjae-lee <karis612@gmail.com>
2021-01-21 16:52:48 +0100

    Revert "move ais-instant-search-ssr to layout"
    This reverts commit 7394dfa180d8070a44dba1b6bed743f92b5eee81.

--- pages/search.vue
+++ pages/search.vue
@@ -1,22 +1,103 @@
 <template>
-  <div>
+  <ais-instant-search-ssr>
     <ais-search-box />
     <ais-hits>
       <div slot="item" slot-scope="{ item }">
         <h2>{{ item.name }}</h2>
       </div>
     </ais-hits>
-  </div>
+  </ais-instant-search-ssr>
 </template>
 
 <script>
-import { AisHits, AisSearchBox } from "vue-instantsearch";
+import {
+  AisInstantSearchSsr,
+  createServerRootMixin,
+  AisHits,
+  AisSearchBox
+} from "vue-instantsearch";
+import algoliasearch from "algoliasearch/lite";
 import "instantsearch.css/themes/algolia-min.css";
+
+const searchClient = algoliasearch(
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
+);
+
+function nuxtRouter(vueRouter) {
+  return {
+    read() {
+      return vueRouter.currentRoute.query;
+    },
+    write(routeState) {
+      vueRouter.push({
+        query: routeState
+      });
+    },
+    createURL(routeState) {
+      return vueRouter.resolve({
+        query: routeState
+      }).href;
+    },
+    onUpdate(cb) {
+      if (typeof window === "undefined") return;
+
+      this._onPopState = event => {
+        const routeState = event.state;
+        // On initial load, the state is read from the URL without
+        // update. Therefore, the state object isn't present. In this
+        // case, we fallback and read the URL.
+        if (!routeState) {
+          cb(this.read());
+        } else {
+          cb(routeState);
+        }
+      };
+      window.addEventListener("popstate", this._onPopState);
+    },
+    dispose() {
+      if (typeof window === "undefined") return;
+
+      window.removeEventListener("popstate", this._onPopState);
+    }
+  };
+}
 
 export default {
   components: {
+    AisInstantSearchSsr,
+    createServerRootMixin,
     AisHits,
     AisSearchBox
+  },
+  data() {
+    const mixin = createServerRootMixin({
+      searchClient,
+      indexName: "instant_search",
+      routing: {
+        router: nuxtRouter(this.$router)
+      }
+    });
+    return {
+      ...mixin.data()
+    };
+  },
+  provide() {
+    return {
+      // Provide the InstantSearch instance for SSR
+      $_ais_ssrInstantSearchInstance: this.instantsearch
+    };
+  },
+  serverPrefetch() {
+    return this.instantsearch.findResultsState(this).then(algoliaState => {
+      this.$ssrContext.nuxt.algoliaState = algoliaState;
+    });
+  },
+  beforeMount() {
+    const results =
+      this.$nuxt.context.nuxtState.algoliaState || window.__NUXT__.algoliaState;
+
+    this.instantsearch.hydrate(results);
   }
 };
 </script>
